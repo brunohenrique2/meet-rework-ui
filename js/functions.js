baffee657c8c588cd80894010f08c810ec3e430e
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2024-03-27 16:51:17 -0300

    update

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,12 @@
 let meetFunctions = document.querySelectorAll(".meetFunctions")
+console.log(meetFunctions[1])
 
 for(let i = 0; i < meetFunctions.length; i++) {
     meetFunctions[i].addEventListener("click", () => {
-      meetFunctions[i].classList.toggle("active")
+        meetFunctions[i].classList.toggle("active")
+        console.log(meetFunctions[i])
+        if(meetFunctions[i].innerHTML == "videocam_off") {
+            meetFunctions[i].innerHTML = "videocam"
+        }
     })
 }
