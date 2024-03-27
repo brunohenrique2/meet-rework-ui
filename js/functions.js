235e0afc6951c995107a551f0855243ffa0fd93c
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2024-03-26 23:10:42 -0300

    update

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+const dockerMeetOptions = document.getElementById("dockerMeetOptions")
+var options = dockerMeetOptions.getElementsByTagName("meetFunctions")
+
+for(var i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", () => {
+
+    })
+}
