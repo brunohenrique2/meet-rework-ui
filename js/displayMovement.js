d4dfbb9e862041d4c692756b6006b0758fca36f1
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2024-04-03 20:09:13 -0300

    update

--- js/displayMovement.js
+++ js/displayMovement.js
@@ -7,9 +7,11 @@ userView.addEventListener("mousedown", (e) => {
     offSetX = e.clientX - userView.offsetLeft
     offSetY = e.clientY - userView.offsetTop
     document.addEventListener("mousemove", move)
+    userView.style.transitionDuration = "0s"
     console.log("pressionando...")
 })
 
 document.addEventListener("mouseup", () => {
+    userView.style.transitionDuration = ".3s"
     document.removeEventListener("mousemove", move)
 })
